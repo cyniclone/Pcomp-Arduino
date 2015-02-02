6f427ec12767cda534aeea5ae847b2fa7a04a9ad
mtgNic <nic.jsb@gmail.com>
2015-02-02 13:07:22 -0500

    Setup and loop method for blinking LED

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,1 +1,14 @@
 int LED = 13;
+
+void setup() 
+{
+  pinMode(LED, OUTPUT);  
+}
+
+void loop()
+{
+   digitalWrite(LED, HIGH);
+   delay(500);    //Delay of 500 ms
+   digitalWrite(LED,LOW);
+   delay(500);
+}
