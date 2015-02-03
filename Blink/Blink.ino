f06278cd5ed7ef06d65d6801241b5b6995095392
mtgNic <nic.jsb@gmail.com>
2015-02-03 16:32:04 -0500

    Added alternating LED blinking

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,14 +1,49 @@
-int LED = 13;
+int LED1 = 10;
+int LED2 = 12;
+boolean randomize = true;
+int myCase = 1;
 
 void setup() 
 {
-  pinMode(LED, OUTPUT);  
+  pinMode(LED1, OUTPUT);  
+  pinMode(LED2, OUTPUT);  
+  Serial.print("hello");
 }
 
 void loop()
 {
-   digitalWrite(LED, HIGH);
-   delay(500);    //Delay of 500 ms
-   digitalWrite(LED,LOW);
-   delay(500);
+  switch (myCase)
+  case 1:
+  // Case 1 - Alternating
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    if (randomize)
+    {
+      delay(random(1,1000));
+    }
+    else
+    { 
+      delay(500);    //Delay of 500 ms
+    }
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    if (randomize)
+    {
+      delay(random(1,1000));
+    }
+    else
+    { 
+      delay(500);    //Delay of 500 ms
+    }
+    break;  
+  }
+
 }
+
+
+
+
+
+
+
